a6cb7ad3b17cac1f076c1a292761866bb7c425d9
Jacob Gallardo-Quintero <39503993+xo909@users.noreply.github.com>
2022-03-25 04:58:09 +0000

    pogger

--- pages/about.js
+++ pages/about.js
@@ -28,7 +28,8 @@ export default function About() {
         <Heading>ABOUT</Heading>
       </Hero>
       <div class="center">
-        <h2>Hello! this is the creator of lila, and i wanted to say this is kinda of a protest agaist schools using NetRef mostly and that it's an invasion of privacy.</h2>
+        <h1>Hello! this is the creator of lila, and i wanted to say this is kinda of a protest agaist schools using NetRef mostly and that it's an invasion of privacy.</h1>
+        <h1>___________________________________________________________</h1>
       </div>
 
     </>
